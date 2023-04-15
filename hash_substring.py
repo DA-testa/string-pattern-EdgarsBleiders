8e88b16f544e7635ab8891bc65c2a46ce47ae16f
EdgarsBleiders <125837153+EdgarsBleiders@users.noreply.github.com>
2023-04-15 22:50:30 +0000

    Edgars Bleiders 221RDB274

--- hash_substring.py
+++ hash_substring.py
@@ -1,29 +1,54 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
     # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    type = input().rstrip()
+
+    if type == "I":
+        pattern = input().rstrip()
+        text = input().rstrip()
+        return (pattern, text)
     
-    
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
-    
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    if type == "F":
+        file = input().rstrip()
+        with open (file, 'r') as f:
+            pattern = f.readline().rstrip()
+            text = f.readline().rstrip()
+            return (pattern, text)
 
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
 def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+    occurr = []
+    P = len(pattern)
+    T = len(text)
 
-    # and return an iterable variable
-    return [0]
+    if T<P:
+        return []
+    
+    Q = 256
+    B = 13
+    pattern_hash = 0
+    current_hash = 0
+    for i in range(P):
+        pattern_hash = (pattern_hash * B + ord(pattern[i])) % Q
+        current_hash = (current_hash * B + ord(text[i])) % Q
+
+    for i in range(T - P + 1):
+        if pattern_hash == current_hash:
+            if pattern == text[i:i+P]:
+                occurr.append(i)
+        
+
+        if i < T - P:
+            current_hash = (B * (current_hash - ord(text[i]) * pow(B, P-1, Q)) + ord(text[i+P])) % Q
+            current_hash = (current_hash + Q) % Q
+
+
+    return occurr        
+
 
 
 # this part launches the functions
